6607417e1d2bd767ebdd7fcd9a09efee5b6dc5c2
Arthur Vianna <arthurvianna@id.uff.br>
2024-02-27 21:52:41 -0300

    feat: achievements highlight

--- frontend/app/components/CartridgeAchievements.tsx
+++ frontend/app/components/CartridgeAchievements.tsx
@@ -1,55 +1,69 @@
+"use client"
+
+
 import AddIcon from '@mui/icons-material/Add';
+import Link from 'next/link'
 import Image from "next/image";
-import { cache } from 'react';
+import { cache, useEffect, useState } from 'react';
 
 import { achievements } from '../libs/achievements/lib';
-import { AchievementInfo } from '../libs/achievements/ifaces';
+import { AchievementInfo, AchievementsPayload } from '../libs/achievements/ifaces';
 import { envClient } from '../utils/clientEnv';
+import { useConnectWallet } from '@web3-onboard/react';
 
 
-const getAchievements = cache(async(id:string) => {
-    let achievementList:Array<AchievementInfo>;
-    if (id.length == 0) {
-        // get all achievements
-        achievementList = (await achievements({}, {cartesiNodeUrl: envClient.CARTESI_NODE_URL, decode: true})).data;
-    } else {
-        // get achievements for a specific cartridge
-        achievementList = (await achievements({cartridge_id: id}, {cartesiNodeUrl: envClient.CARTESI_NODE_URL, decode: true})).data;
-    }
+const getAchievements = cache(async(id:string, user:string) => {
+    let parameters:AchievementsPayload = {};
+    if (id.length > 0) parameters.cartridge_id = id;
+    if (user.length > 0) parameters.player = user;
+
+    const achievementList:Array<AchievementInfo> = (await achievements(parameters, {cartesiNodeUrl: envClient.CARTESI_NODE_URL, decode: true})).data;
 
     return achievementList;
 })
 
 
 
-export default async function CartridgeAchievements({cartridge_id}:{cartridge_id?:string}) {
+export default function CartridgeAchievements({cartridge_id}:{cartridge_id?:string}) {
+    const [achievements, setAchievements] = useState<Array<AchievementInfo>|null>(null);
+    const [{ wallet }] = useConnectWallet();
+
     const selectedCartridgeId = cartridge_id || "";
-    const achievements = await getAchievements(selectedCartridgeId);
+    const user = wallet? wallet.accounts[0].address: "";
+
+    useEffect(() => {
+        getAchievements(selectedCartridgeId, user).then((achievementList) => setAchievements(achievementList));
+    }, [wallet?.accounts[0]])
 
 
     return (
         <div className="element rounded grid grid-cols-6 items-center justify-items-center">
-            <a className="flex flex-col items-center p-4 text-center hover-color" href={`/achievement/create/${selectedCartridgeId}`}>
+            <Link className="flex flex-col items-center p-4 text-center hover-color" href={`/achievement/create/${selectedCartridgeId}`}>
                 <div className="border-4 rounded-full">
                     <AddIcon style={{width: "128px", height: "128px"}}/>
                 </div>
                 <span className="text-2xl">Create Achievement</span>
-            </a>
+            </Link>
 
             {
-                achievements.map((achievement, index) => {
-                    return (
-                        <a key={achievement.id} className="flex flex-col items-center p-4 text-center hover-color" href={`/achievement/${achievement.id}`}>
-                            <Image className="rounded-full border-4"
-                                alt={achievement.name}
-                                src={achievement.icon? `data:image/png;base64,${achievement.icon}`:"/made_it_symbol_trans.png"}
-                                width={128}
-                                height={128}
-                            />
-                            <span className="text-2xl">{achievement.name}</span>
-                        </a>
-                    )
-                })
+                !achievements?
+                    <div className="btn w-full p-4 flex justify-center element">
+                        <div className='w-16 h-16 border-2 rounded-full border-current border-r-transparent animate-spin'></div>
+                    </div>
+                :
+                    achievements.map((achievement, index) => {
+                        return (
+                            <Link key={achievement.id} className={`flex flex-col items-center p-4 text-center hover-color ${achievement.player_achieved? "": "opacity-50" }`} href={`/achievement/${achievement.id}`}>
+                                <Image className="rounded-full border-4"
+                                    alt={achievement.name}
+                                    src={achievement.icon? `data:image/png;base64,${achievement.icon}`:"/made_it_symbol_trans.png"}
+                                    width={128}
+                                    height={128}
+                                />
+                                <span className="text-2xl">{achievement.name}</span>
+                            </Link>
+                        )
+                    })
             }
         </div>
     )

01712f8d5792f6f43c12c01aca8259939bfd2ffc
Lyno Ferraz <lynoFerraz@gmail.com>
2024-03-02 09:42:03 -0700

    fix: Small fixes on swap component

--- frontend/app/components/DepositModal.tsx
+++ frontend/app/components/DepositModal.tsx
@@ -5,7 +5,7 @@ import { cache, useEffect, useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { envClient } from '../utils/clientEnv';
 import { useConnectWallet } from '@web3-onboard/react';
-import { ContractReceipt, ethers } from 'ethers';
+import { BigNumber, ContractReceipt, ethers } from 'ethers';
 import WarningIcon from '@mui/icons-material/Warning';
 import { depositErc20 } from '../libs/wallet/lib';
 
@@ -16,14 +16,17 @@ const USDC_ADDR = "0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8";
 
 
 const getQuote = cache(async function getPrice(sellTokenAddr:string, buyTokenAddr:string, amount:number){
-    amount = amount * 10**18;    
+    // amount = amount * 10**18;    
+    let correctedAmount = BigNumber.from(amount);
+    correctedAmount = ethers.utils.parseUnits(`${amount}`,18);
+
       
     // This is a placeholder. Get your live API key from the 0x Dashboard (https://dashboard.0x.org/apps)
     const headers = {"0x-api-key": "a89700a3-518d-4e73-8125-d5a1a44b2527"};
     
     // Fetch the swap price.
     const response = await fetch(
-    `https://sepolia.api.0x.org/swap/v1/price?sellToken=${sellTokenAddr}&buyToken=${buyTokenAddr}&sellAmount=${amount}`, { headers }
+    `https://sepolia.api.0x.org/swap/v1/price?sellToken=${sellTokenAddr}&buyToken=${buyTokenAddr}&sellAmount=${correctedAmount.toString()}`, { headers }
     );
 
     if (response.status !== 200) {console.log("0x quote obtained"); return null;}
@@ -60,8 +63,9 @@ export default function DepositModal({option}:{option:MODAL_OPTIONS}) {
     const handleQuote = () => {
         if (!sellAmount) return;
         getQuote(DAI_ADDR, USDC_ADDR, sellAmount).then((res) => {
+            console.log(res)
             setLastQuote({
-                buyAmount: res.buyAmount / 10*(envClient.ACCPTED_TOKEN_DECIMALS),
+                buyAmount: parseFloat(ethers.utils.formatUnits(BigNumber.from(res.buyAmount),envClient.ACCPTED_TOKEN_DECIMALS)),
                 estimatedGas: res.estimatedGas,
                 allowanceTarget: res.allowanceTarget
             });
@@ -79,6 +83,8 @@ export default function DepositModal({option}:{option:MODAL_OPTIONS}) {
             alert("Quote the prices first");
             return;
         }
+        let correctedSellAmount = BigNumber.from(sellAmount);
+        correctedSellAmount = ethers.utils.parseUnits(`${sellAmount}`,18);
 
         const erc20abi= [{ "inputs": [ { "internalType": "string", "name": "name", "type": "string" }, { "internalType": "string", "name": "symbol", "type": "string" }, { "internalType": "uint256", "name": "max_supply", "type": "uint256" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" } ], "name": "allowance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "approve", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "burn", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "account", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "burnFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [ { "internalType": "uint8", "name": "", "type": "uint8" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" } ], "name": "decreaseAllowance", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" } ], "name": "increaseAllowance", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transfer", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }]
         const signer = new ethers.providers.Web3Provider(wallet.provider, 'any').getSigner();
@@ -86,22 +92,30 @@ export default function DepositModal({option}:{option:MODAL_OPTIONS}) {
 
         // // allowance
         try {
-            setIsLoading(true);
-            const approveTx = await sellTokenContract.approve(lastQuote.allowanceTarget, sellAmount);
-            const approveReceipt = await approveTx.wait();
-            console.log(approveReceipt);
-            setIsLoading(false);
+            const signerAddress = await signer.getAddress();
+            const allowance = await sellTokenContract.allowance(
+                signerAddress,
+                DAI_ADDR
+            );
+            if (allowance.lt(correctedSellAmount)) {
+                setIsLoading(true);
+                const allowanceApproveAmount = correctedSellAmount.sub(allowance);
+                const approveTx = await sellTokenContract.approve(lastQuote.allowanceTarget, allowanceApproveAmount);
+                const approveReceipt = await approveTx.wait();
+                console.log(approveReceipt);
+                setIsLoading(false);
+            }
         } catch(error) {
             console.log((error as Error).message);
             return;
         }
 
         // swap
-        const amount = sellAmount * 10 ** 18;
+        // const amount = sellAmount * 10 ** 18;
         // This is a placeholder. Get your live API key from the 0x Dashboard (https://dashboard.0x.org/apps)
         const headers = {"0x-api-key": "a89700a3-518d-4e73-8125-d5a1a44b2527"};
         const response = await fetch(
-            `https://sepolia.api.0x.org/swap/v1/quote?sellToken=${DAI_ADDR}&buyToken=${USDC_ADDR}&sellAmount=${amount}`, { headers }
+            `https://sepolia.api.0x.org/swap/v1/quote?sellToken=${DAI_ADDR}&buyToken=${USDC_ADDR}&sellAmount=${correctedSellAmount.toString()}`, { headers }
         );
 
         const response_json = await response.json();
